07cb59cf673071a1bd0748825be170c6efbb2a77
Suhaib <suhaibsworkspace@gmail.com>
2023-05-17 14:11:17 +0530

    Updated UI

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
 const apiKey = "c1ab2c00ee8700801e0052c1001c8f54";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric";
 
-async function getWeather(city) {
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+async function getForecast(city) {
     const response = await fetch(apiUrl + "&appid=" + apiKey + "&q=" + city);
     var data = await response.json();
     if (data.cod == 200) updateValues(data);
     else if (data.cod == 404) alert("City Not Found");
     else alert("Error fetching data from API")
+
+    console.log(data)
 }
 
 function $(id) {
     return document.getElementById(id);
 }
 
-const updateValues = (data) => {
-    const date = new Date(data.dt * 1000)
-    const sunrise = new Date(data.sys.sunrise * 1000);
-    const sunset = new Date(data.sys.sunset * 1000);
-    $("currentDate").innerText = date.getDate() + "/" + (date.getMonth() + 1).toString().padStart(2, 0);
-    $("currentTime").innerText = date.getHours() + ":" + date.getMinutes();
-    $("weatherIcon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    $("currentWeather").style.background = `url('images/${data.weather[0].icon}.jpg') no-repeat center center/cover`;
-    $("temperature").innerText = data.main.temp.toFixed(0);
-    $("description").innerText = data.weather[0].main;
-    $("feelTemp").innerText = data.main.feels_like.toFixed(0);
-    $("sunrise").innerText = sunrise.getHours().toString().padStart(2, "0") + ":" + sunrise.getMinutes().toString().padStart(2, "0");
-    $("sunset").innerText = sunset.getHours().toString().padStart(2, "0") + ":" + sunset.getMinutes().toString().padStart(2, "0");
-    $("city").innerText = data.name;
-    $("wind").innerText = data.wind.speed + " m/s";
-    $("humidity").innerText = data.main.humidity + " %";
-    $("pressure").innerText = data.main.pressure + " mb"
-    $("map").src = `https://maps.google.com/maps/place?q=${data.name}&output=embed`;
+function getTime(time) {
+    let T = new Date(time * 1000);
 
+    if (T.getHours() > 12) {
+        return (T.getHours() - 12) + ":" + T.getMinutes() + " PM";
+    }
+    return T.getHours() + " : " + T.getMinutes() + " AM";
 }
 
-$("form").addEventListener('submit', changeLocation);
+const updateValues = (data) => {
+    let D = new Date();
+    $('location').innerText = data.city.name;
+    $("weatherIcon").src = `icons/${data.list[0].weather[0].icon}.svg`;
+    $('temperature').innerText = Math.round(data.list[0].main.temp);
+    $('dateTime').innerHTML = "Time: " + D.getHours() + " : " + D.getMinutes() + "<br>" + "Date: " + D.getDate() + " / " + (D.getMonth() + 1)
+    $("currentWeather").style.background = `url('images/${data.list[0].weather[0].icon}.jpg') no-repeat center center/cover`;
+    $('sunrise').innerText = getTime(data.city.sunrise);
+    $('sunset').innerText = getTime(data.city.sunset);
+    $('humidity').innerText = data.list[0].main.humidity + "%";
+    $('pressure').innerText = data.list[0].main.pressure + " hPa";
+    $('wind').innerText = data.list[0].wind.speed + " m/s";
+    $('level').innerText = data.list[0].main.grnd_level + " hPa";
+    $('visiblity').innerText = data.list[0].visibility + " m";
+
+    // Update forecast
+
+    const forecastTiles = $('forecastContainer').children;
+    for (let i = 0; i < 5; i++) {
+        console.log(i)
+        console.log(forecastTiles[i].children[0])
+        forecastTiles[i].children[0].src = `icons/${data.list[i * 8].weather[0].icon}.svg`;
+        forecastTiles[i].children[1].innerHTML = weekday[D.getDay() + i];
+        forecastTiles[i].children[2].innerHTML = Math.round(data.list[i].main.temp) + "&#8451;";
+    }
+}
+
 function changeLocation(event) {
     event.preventDefault();
     const cityName = $("locationInput").value;
-    getWeather(cityName);
+    getForecast(cityName);
 }
 window.onload = () => {
-    getWeather('Srinagar');
+    getForecast('Srinagar');
+    $("form").addEventListener('submit', changeLocation);
 }
